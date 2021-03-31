975fb052f8565c1b57fdac536738abd5e4781737
Rick <rmansfield@realpeoplesratings.com>
2021-03-31 12:59:08 -0500

    Update Quizes 1-2

---
+++ Precourse/CSS/Quiz1/js.js
@@ -0,0 +1,47 @@
+HTML CSS JSResult Skip Results Iframe
+/*
+Exercise - Selector Quiz
+
+START HERE
+
+Answer the questions below based on the HTML / CSS to the left. Do not manipulate the HTML or CSS in any way until after you have completed the quiz (no peeking!). Afterwards you may delete line 1 from the CSS to check your work (reminder this is not graded! Try and compelte this without looking). 
+
+1. Examine the h3 element containing "Step 1". What is the text color? What is the background color?
+Text color: aqua;
+background-color: black;
+
+2. Look at the h3 element containing "Step 3". What is the text and background color of this element?
+color: orange;
+background-color: black;
+
+3. Look at all four paragraphs. What is the text color of each of them?
+color: lightgrey;
+color: lightgrey;
+color: white;
+
+4. What is the border color around the first div element? 
+border: 3px solid red;
+
+
+5. What is the background color for the second div element?
+none or white
+
+6. What is the background color for each of the three p elements with the class "step-text"?
+blue 
+blue
+red
+
+
+Stretch Questions:
+
+7. What is the text and background color of the h1 element?
+  background-color: blue;
+  color: white;
+
+8. Of the p elements, which is the largest (takes up the most space on the page)?
+padding on the final one makes it largest. 
+*/
+
+
+
+Resources1× 0.5× 0.25×Rerun
