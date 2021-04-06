b63e51449b0efa2655b8e157afb74f35ec440331
Rick <rmansfield@realpeoplesratings.com>
2021-04-06 10:51:01 -0500

    Update Control Flow Coin Flip Exercises

---
+++ Precourse/Control_Flow/Coin_Flip/index.js
@@ -0,0 +1,15 @@
+var coinFlip = Math.round(Math.random());
+
+if (coinFlip === 1) {
+    console.log('Heads')
+} else {
+    console.log('Tails');
+}
+
+// //Alternative
+// if (coinFlip === 1) {
+//     console.log('Heads')
+// }
+// if (coinFlip === 0) {
+//     console.log('Tails')
+// }
---
+++ Precourse/Control_Flow/Coin_Flip_with_Loop/index.js
@@ -0,0 +1,14 @@
+
+
+// add loop to flip coin 5x
+
+for (let count = 0; count < 6; count++) {
+    var coinFlip = Math.round(Math.random());
+    if (coinFlip === 1) {
+    console.log(count + " Heads");
+    }
+
+    if (coinFlip === 0) {
+    console.log(count + " Tails");
+    }
+}
