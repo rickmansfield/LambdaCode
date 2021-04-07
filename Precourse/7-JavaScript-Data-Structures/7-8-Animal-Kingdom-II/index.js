93c5e13577f46c2a7c75620ff7603144c3e93b86
Rick <rmansfield@realpeoplesratings.com>
2021-04-07 18:45:25 -0500

    update 7-7 thru 7-9

---
+++ Precourse/7-JavaScript-Data-Structures/7-8-Animal-Kingdom-II/index.js
@@ -0,0 +1,15 @@
+const greatWhiteShark = {
+    myKingdom: 'Animalia',
+    myPhylum: 'Chordata',
+    myClass: 'Chondrichthyes',
+    myOrder: 'Lamniformes',
+    myGenus: 'Charcharodon',
+    mySpecies: 'charcharias',
+    // add method here 
+    getScientificName: function(){
+        let scientificName = this.myGenus + ' ' + this.mySpecies;
+        return(scientificName);
+    }
+}
+
+console.log(greatWhiteShark.getScientificName());
