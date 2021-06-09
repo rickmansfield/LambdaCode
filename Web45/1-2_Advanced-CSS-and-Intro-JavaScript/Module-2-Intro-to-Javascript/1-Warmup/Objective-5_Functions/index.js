31e70de60204464182ddbe9eb1c582d5f3b58fd2
Rick <rmansfield@realpeoplesratings.com>
2021-06-08 19:45:54 -0500

    Various Exercises 06082021 at 742pm

---
+++ Web45/1-2_Advanced-CSS-and-Intro-JavaScript/Module-2-Intro-to-Javascript/1-Warmup/Objective-5_Functions/index.js
@@ -0,0 +1,8 @@
+// Create a function to console.log the steps to your favorite recipie
+functio recipies() {
+    console.log('steps here');
+    
+}
+
+// Use the space below to declare the same function as an arrow function 
+const recipies => () console.log('');
---
+++ Web45/1-2_Advanced-CSS-and-Intro-JavaScript/Module-2-Intro-to-Javascript/3-Review/Quiz/index.js
@@ -0,0 +1,19 @@
+// let a = (2+3) * (4+5)
+// console.log(a);
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+//    }
+//  next question
+// var a = 10;
+// if (a === 10) {
+//  console.log("it's true");
+// } else {
+//  console.log("it's false");
+// }
+
+function myFunction(){
+    let alert =('hello');
+   };
+
+   myFunction();
