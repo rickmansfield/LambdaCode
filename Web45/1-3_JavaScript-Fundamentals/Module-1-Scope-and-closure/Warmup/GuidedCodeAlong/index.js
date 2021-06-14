36184778ca79ba4b35d861da6878379245037eb8
Rick <rmansfield@realpeoplesratings.com>
2021-06-14 14:18:12 -0500

    Home work updates

--- Web45/1-3_JavaScript-Fundamentals/Module-1-Scope-and-closure/Warmup/GuidedCodeAlong/index.js
+++ Web45/1-3_JavaScript-Fundamentals/Module-1-Scope-and-closure/Warmup/GuidedCodeAlong/index.js
@@ -2,4 +2,27 @@ function score(){
     return Math.floor((Math.random() * 5) + 1);
 }
 
-console.log(score());
+// console.log(score());
+
+function hockeyGame(scoreCB){
+    return {
+        Home: scoreCB(),
+        Away: scoreCB(),
+    };
+}
+// console.log(hockeyGame(score));
+
+
+function totalGameScore(scorecb, gamecb){
+    const totalGame = [];
+    let homeScore = 0;
+    let awayScore = 0;
+    for (let i = 0; i < 3; i++){
+        const currentScore = gamecb(scorecb);
+        homeScore = homeScore + currentScore.Home;
+        awayScore = awayScore + currentScore.Away;
+        totalGame.push(`perriod ${i + 1}: Away: ${currentScore.Away} - Home: ${currentScore.home}`);
+    }
+    return totalGame;
+}
+console.log(totalGameScore(score, hockeyGame));
