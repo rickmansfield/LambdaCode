9dc837944894362f3f4c557d9629733ab8d2e2c3
Rick <rmansfield@realpeoplesratings.com>
2021-06-28 18:54:00 -0500

    added to HTML and updated index.js

---
+++ Web45/Unit2/2-1/1Warmup/Objective2/index.js
@@ -0,0 +1,45 @@
+/*
+
+  For this challenge you will be asked to follow the requests below and decipher which selector method you should use.  
+  
+  Once you have finished, you can check your work here:  https://codepen.io/lambdaschool/pen/OoYqNM?editors=1010
+  
+*/ 
+
+/*
+  Request 1: Log the result of every `a` element on the page. The result should be an `HTMLCollection`
+*/
+
+//const anchorTags;
+//console.log(anchorTags);
+
+/* 
+  Request 2: Log the result of every `a` element on the page. The result should be a `NodeList`
+*/
+
+// const moreAnchorTags;
+// console.log(moreAnchorTags);
+
+/* 
+  Request 3: Locate the ID in the HTML. Log the element that the ID is on.
+*/
+
+// const customID;
+// console.log(customID);
+
+/* 
+  Request 4: Log the first class that matches ".main-content"
+*/
+
+// const mainContent;
+// console.log(mainContent);
+
+/* 
+  Request 5: Log the third element with a class of ".nav-item"
+*/
+
+// const thirdNavItem;
+// console.log(thirdNavItem);
+
+
+
