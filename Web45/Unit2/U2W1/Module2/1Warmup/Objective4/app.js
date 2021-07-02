1eb40fa86c61e1687d017aee90ab2021aae7f2fa
Rick <rmansfield@realpeoplesratings.com>
2021-07-02 15:08:06 -0500

    u2 m2 ob5 step 12 complete

--- Web45/Unit2/U2W1/Module2/1Warmup/Objective4/app.js
+++ Web45/Unit2/U2W1/Module2/1Warmup/Objective4/app.js
@@ -62,7 +62,8 @@ button2.addEventListener('mouseleave', event => {
 
 /* Step Eight: Finish the section in TK on stopPropagation */
 function listener(event) {
-  console.log(`event passing through ${event.currentTarget.nodeName || 'Window'}`);
+  console.log(`event passing through ${event.currentTarget.nodeName || 'Window'} target --> ${event.target.nodeName}`
+  );
 }
 
 button2.addEventListener('click', listener);//should read "event passing through DIV." The video (for THEIR example) says "event passing through BUTTON" is the inner most target. But in this case (inpsect index.html) the inner most target is a <div> inside another <div> not a <button> inside a <div>
@@ -72,8 +73,8 @@ button2.addEventListener('click', listener);//should read "event passing through
 const container = document.querySelector('.container');
 console.log(container);
 container.addEventListener('click', event => {
-  event.target.style.boarder = 'purple';
-  event.target.style.boarder = '2px';
+  event.target.style.border = 'purple';
+  event.target.style.border = '5px';
   event.target.style.backgroundColor = 'pink';
 });
 
@@ -82,13 +83,25 @@ container.addEventListener('click', event => {
 /* Step Ten: Click the button, notice how the container click event also triggers? */
 
 //Answer=========================================
-//not sure how userfull this is.. but ok I get it. The most deeply nested element fires first and then the outtermost fires after that. Cool? or Annoying? lol
+//not sure how userfull this is.. but ok I get it. Order listeners are added to separate elements doesn't make any difference. The most **deeply nested element fires first** and then the next outtermost fires after that and so on. Cool? or Annoying? lol
 window.addEventListener('click', listener);
 document.addEventListener('click', listener);
+const body = document.querySelector('body');
+button2.addEventListener('click', (e) => e.stopPropagation());
+body.addEventListener('click', listener);
 button2.addEventListener('click', listener);
 
+//However order in which listeners are added to the SAME element IS important. They fire in the order they were added due to the "bubbling phase" post the capture save. The event goes straight to the target phase and then the bubbling occurs phase. 
+button2.addEventListener('click', e => console.log('click!'));
+
+//This can be interrupted with hard coded event firing insdie the code. Hence the reason for the next step.
 
 /* Step Eleven: Add stopPropagation to the button click handler */
+// button2.addEventListener('click', (e) => e.stopPropagation());
 
 /* Step Twelve: Click the button now, the click should not propagate to the conainer. */
+
+//Answer======================
+//worked great. I posed the answer in step eleven and copied it into step 10 to comment it out (turn it on/off) and "body" would stop being propagated while it was on during a click. i.e. the event listener for body would not propagate since the .stopPropagatio() was place BEFORE that point in the code. 
+//Coolieo... 
 /* All Done! */
