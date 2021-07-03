80852414f9db28dbc693bdab50aca68833c5f0e0
Rick <rmansfield@realpeoplesratings.com>
2021-07-03 10:50:10 -0500

    add mouse over event

---
+++ Web45/Unit2/U2W1/Module2/2Engage/DailyProject/index.js
@@ -0,0 +1,10 @@
+//1 Mouseover <h1> yellow up
+const headerH1 = document.querySelector('h1');
+console.log(headerH1
+    );
+    headerH1.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = 'yellow'; 
+    });
+    headerH1.addEventListener('mouseleave', event => {
+        event.target.style.backgroundColor = 'white';
+    });
