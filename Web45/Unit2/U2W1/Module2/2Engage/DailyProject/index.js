b81496739513a6f900edb07ef1c22a8c7e9c0fc7
Rick <rmansfield@realpeoplesratings.com>
2021-07-03 10:51:56 -0500

    u2 m2 engage added commnets

--- Web45/Unit2/U2W1/Module2/2Engage/DailyProject/index.js
+++ Web45/Unit2/U2W1/Module2/2Engage/DailyProject/index.js
@@ -8,3 +8,10 @@ console.log(headerH1
     headerH1.addEventListener('mouseleave', event => {
         event.target.style.backgroundColor = 'white';
     });
+
+    //2 Keydown <h4> "Sign Me Up!" 
+//First note I'm using <div class="btn>Sign Me Up!</> as <button id = "launchButton">Launch!</button> from web guided project.I want to use "Esc" key 27 to leave the modal as my Keydown event. 
+
+//Isolate <section class="content-pick"> to add needed elements
+// const sectionContentPick = document.querySelector('content-pick');
+// console.log(sectionContentPick);
