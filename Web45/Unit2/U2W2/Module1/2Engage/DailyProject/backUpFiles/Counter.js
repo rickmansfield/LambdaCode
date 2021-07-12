99f085f52e322109defe75749151aceca78795b1
Rick <rmansfield@realpeoplesratings.com>
2021-07-12 12:50:34 -0500

    U2.2M2 Homework Moods.js all test passing

--- Web45/Unit2/U2W2/Module1/2Engage/DailyProject/backUpFiles/Counter.js
+++ Web45/Unit2/U2W2/Module1/2Engage/DailyProject/backUpFiles/Counter.js
@@ -51,7 +51,7 @@ import React, {useState} from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
   const [count, setCount] = useState(0);
-  const even = (count % 2 === 0)
+  const even = (count % 2 === 0);
 
   const increment = () => {
     setCount( count + 1 );
