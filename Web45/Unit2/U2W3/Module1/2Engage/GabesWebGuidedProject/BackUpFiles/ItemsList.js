266f157abf2a6fa6a231a13b9cfb27bf88fe2bd8
Rick <rmansfield@realpeoplesratings.com>
2021-07-18 14:47:09 -0500

    U2.3 M1 Gabes Video Step 6 complete

---
+++ Web45/Unit2/U2W3/Module1/2Engage/GabesWebGuidedProject/BackUpFiles/ItemsList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+// We'll need a Link and the useRouteMatch hook from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
+
+export default function ItemsList(props) {
+  const { items } = props
+    // We'll grab the current URL using the hook
+  //returns an object with things, among them the current url so we don't ahve to repeat it in our Links. 
+  const { url } =useRouteMatch();
+
+  return (
+    <div className='items-list-wrapper'>
+      {items.map(item => (
+        <div
+          className='item-card'
+          key={item.id}
+        >
+          {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
+          <Link to={`${url}/${item.id}`}>
+          <img
+            className='items-list-image'
+            src={item.imageUrl}
+            alt={item.name}
+          />
+          <p>{item.name}</p>
+          </Link>
+          {/* Link ends */}
+
+          <p>${item.price}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
