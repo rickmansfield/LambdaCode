b5571acfa714ed8515c8dad113a790fcc0058165
Rick <rmansfield@realpeoplesratings.com>
2021-07-20 13:48:53 -0500

    U2.3 M3 Start Onboarding Proj App.js file

---
+++ Web45/Unit2/U2W3/Module3/2Engage/DailyProject/OnboardingBackup/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+            <Form
+        // values={formValues} 
+        // update={updateForm} 
+        // submit={submitForm}
+      />
+    </div>
+    
+  );
+}
+
+export default App;
