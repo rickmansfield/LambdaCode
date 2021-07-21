036f3e3829d5831c857faf55623b3c7599e995db
Rick <rmansfield@realpeoplesratings.com>
2021-07-21 08:17:33 -0500

    U2.3 m1 SavedList.js stretch backup

--- Web45/Unit2/U2W3/Module1/2Engage/DailyProject/React-Router-Movies/Movies/SavedList.js
+++ Web45/Unit2/U2W3/Module1/2Engage/DailyProject/React-Router-Movies/Movies/SavedList.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+  const history = useHistory()
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <span key={movie.id} className="saved-movie">{movie.title}</span>
       ))}
       
-      <Link to='/'><div className="home-button" >Home</div></Link>
+      <Link to='/'><div className="home-button" onClick={() => history.push('/')}>Home</div></Link>
     </div>
   );
 }
